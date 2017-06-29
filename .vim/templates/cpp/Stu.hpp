579b39b4f45c2429a34a5b1534719e90dabfd75e
Crane <215379951@qq.com>
2017-06-29 12:54:15 +0800

    add Person in Stu.hpp
    +" rstu stand for read Stu.hpp
    +autocmd   FileType  c,cpp   inoreabbrev rstu <ESC>:read $TEMPLATE_CPP/Stu.hpp<CR>

--- .vim/templates/cpp/Stu.hpp
+++ .vim/templates/cpp/Stu.hpp
@@ -3,17 +3,35 @@
 #include <string>
 #include <memory>
 
-class Stu
+class Person
+{
+public:
+    Person() {
+
+    }
+
+};
+
+class Stu : public Person
 {
     typedef std::string string;
 
 public:
-    Stu(const string &name) : name_(name) {
+    Stu(const string &name = string("") ) : name_(name) {
         std::cout << "Stu() by string ..." << std::endl;
     }
 
     Stu(const char *name)   : name_(name) {
         std::cout << "Stu() by const char * ..." << std::endl;
+    }
+
+    // copy constructor
+    Stu(const Stu &stu) : name_(stu.name_){
+        std::cout << "copy Stu() by  name " << name_ << std::endl;
+    }
+
+    void show() {
+        std::cout << name_ << std::endl;
     }
 
     ~Stu() {
