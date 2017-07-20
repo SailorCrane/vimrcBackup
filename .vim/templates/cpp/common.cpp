168c87474f13cb1a929ef4223286d12398705202
Crane <215379951@qq.com>
2017-07-20 10:13:37 +0800

    in .vim/templates/cpp/common.cpp
    add some common code

--- .vim/templates/cpp/common.cpp
+++ .vim/templates/cpp/common.cpp
@@ -1,13 +1,21 @@
-
 #include <iostream>
 #include <vector>
 
+// 获取数组长度
+#define  ArraySize(array)       ( sizeof(array) / sizeof( array[0] ) )
+
 typedef  std::vector<int>  VInt;
+
+void showVector(const  VInt & ins)
+{
+    for( auto ele : ins )
+        std::cout << ele << std::endl;
+    std::cout << std::endl;
+}
 
 int main(int argc, char **argv)
 {
     std::cout << "running..." << std::endl;
-
 
 
     std::cout << "end..." << std::endl;
