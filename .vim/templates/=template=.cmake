7d85eaa05ffc769be3c3cf1c00f32c3c12931d91
Crane <215379951@qq.com>
2017-07-31 19:16:01 +0800

    in template/ CMakeLists.txt linke to .cmake

--- .vim/templates/=template=.cmake
+++ .vim/templates/=template=.cmake
@@ -1,13 +1,26 @@
+# vim: set filetype=cmake  : vim modeline( set )
 cmake_minimum_required (VERSION 2.8)
 
-# projectname is the same as the main-executable
 project(%HERE%%FDIR%)
 
 add_definitions('-g')
 add_definitions('-Wall')
-#add_definitions('-std=c++11')
+add_definitions('-std=c++11')
 
-add_executable(${PROJECT_NAME} ${PROJECT_NAME}.cpp)
+# ============find-so-lib=================
 
-add_custom_target(${PROJECT_NAME}-symlink ALL ln --force -s ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME} ${CMAKE_SOURCE_DIR}/${PROJECT_NAME} DEPENDS ${PROJECT_NAME})
-set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES ${CMAKE_SOURCE_DIR}/${PROJECT_NAME})
+include_directories(
+                      )
+
+link_directories(
+                   )
+
+# ============ target =================
+
+set(TARGET main)
+add_executable(${TARGET}  ${TARGET}.cpp)
+
+#add_library(${TARGET}  SHARED ${TARGET}.cpp)
+
+#set_target_properties (target PROPERTIES COMPILE_FLAGS "-fPIC")    # need -fPIC!!!
+#set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES ${CMAKE_SOURCE_DIR}/${PROJECT_NAME})
