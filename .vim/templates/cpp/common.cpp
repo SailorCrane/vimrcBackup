9ebab4ec7017b0aa5f40d0521ab3e23c2158f544
Crane <215379951@qq.com>
2017-08-03 16:39:54 +0800

    changed cpp/common.cpp

--- .vim/templates/cpp/common.cpp
+++ .vim/templates/cpp/common.cpp
@@ -4,13 +4,18 @@
 
 #include <numeric>
 #include <algorithm>
+#include <typeinfo>
 
 // 获取数组长度
 #define  ArraySize(array)       ( sizeof(array) / sizeof( array[0] ) )
+#define  TypeName(value)        ( typeid(value).name() )
 
 typedef  std::vector<int>  VInt;
 typedef  std::list<int>    LInt;
 
+
+// 这里参数为引用, 可以保证类型不退化: 比如数组类型不会退化为指针
+// 这里可以接受数据类型
 template<class Containor>
 void showContain(const  Containor & ins)
 {
